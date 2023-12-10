9093a6c29f5c5f92611a7a25d5002b5b983d86eb
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-12-10 22:11:21 +0300

    added log

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] array = {2, 5, 13, 7, 6, 4};
+        System.out.println(intAve(array));
+    }
+
+    public static float intAve(int[] arr) {
+        int arrSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            arrSum += arr[i];
+        }
+        return (float) arrSum / arr.length;
     }
 }
